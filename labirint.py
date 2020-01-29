84075223f8a7c79d6167b6afd835c1fc82c31ac5
Богдана <bogdana.nekrasova@gmail.com>
2020-01-29 23:41:05 +0700

    лабиринт

--- labirint.py
+++ labirint.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import random
 from pygame.locals import K_UP, K_DOWN, K_LEFT, K_RIGHT
 
 SIZE = WIDTH, HEIGHT = 670, 800
@@ -26,7 +27,8 @@ def start_screen(game_over=False):
     if not game_over:
         intro_text = ["Правила игры:",
                       "Перемещайтесь с помощью стрелок.",
-                      "Не наступайте на клетки с коробками."]
+                      "Не наступайте на клетки с коробками.",
+                      "Нажмите Enter для началы игры."]
     else:
         intro_text = ["GAME OVER",
                       "Нажмите Esc для выхода ",
@@ -34,7 +36,7 @@ def start_screen(game_over=False):
         global score, text, font
         score = 0
         text = font.render(str(score), 1, pygame.Color('black'))
-    fon = pygame.transform.scale(images['fon2'], (350, 400))
+    fon = pygame.transform.scale(images['fon'], (350, 400))
     screen.blit(fon, (150, 270))
     font = pygame.font.Font(None, 25)
     text_coord = 330
@@ -43,7 +45,7 @@ def start_screen(game_over=False):
         intro_rect = string_rendered.get_rect()
         text_coord += 10
         intro_rect.top = text_coord
-        intro_rect.x = 190
+        intro_rect.x = 185
         text_coord += intro_rect.height
         screen.blit(string_rendered, intro_rect)
         screen.blit(images['egg'], (0, 0))
@@ -71,18 +73,16 @@ def load_level(filename):
     return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 
-images = {'wall': load_image('box.png', -1), 'empty': load_image('grass.png', -1),
-          'persona': load_image('hamster_lab.png', -1), 'egg': load_image('egg.png', -1)}
-
-tile_width = tile_height = 50
-
-
 class Tile(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(tiles_group, all_sprites)
         self.image = images[tile_type]
         self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
         self.add(tiles_group, all_sprites)
+        if tile_type == 'wall':
+            self.add(box_group)
+        elif tile_type == 'heart':
+            self.add(heart_group)
 
 
 class Player(pygame.sprite.Sprite):
@@ -90,11 +90,6 @@ class Player(pygame.sprite.Sprite):
         super().__init__(player_group, all_sprites)
         self.image = images['persona']
         self.rect = self.image.get_rect().move(tile_width * pos_x, tile_height * pos_y)
-
-
-all_sprites = pygame.sprite.Group()
-tiles_group = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
 
 
 def generate_level(level):
@@ -108,70 +103,129 @@ def generate_level(level):
             elif level[y][x] == '@':
                 Tile('empty', x, y)
                 new_player = Player(x, y)
+            elif level[y][x] == '&':
+                Tile('heart', x, y)
+                Tile('empty', x, y)
     return new_player, x, y
 
 
 class Camera:
-    # зададим начальный сдвиг камеры
-    def __init__(self):
+    def __init__(self, size):
         self.dx = 0
         self.dy = 0
+        self.size = size
 
-    # сдвинуть объект obj на смещение камеры
     def apply(self, obj):
         obj.rect.x += self.dx
-        obj.rect.y += self.dy
 
         if obj.rect.x < -obj.rect.width:
-            obj.rect.x += (10 + 1) * obj.rect.width
+            obj.rect.x += (self.size[0] + 1) * obj.rect.width
 
-        if obj.rect.x >= 10 * obj.rect.width:
-            obj.rect.x += -obj.rect.width * (1 + 10)
+        if obj.rect.x >= (self.size[0]) * obj.rect.width:
+            obj.rect.x += -obj.rect.width * (1 + self.size[0])
+        obj.rect.y += self.dy
 
         if obj.rect.y < -obj.rect.height:
-            obj.rect.y += (10 + 1) * obj.rect.height
-        if obj.rect.y >= 10 * obj.rect.height:
-            obj.rect.y += -obj.rect.height * (1 + 10)
+            obj.rect.y += (self.size[1] + 1) * obj.rect.height
+        if obj.rect.y >= (self.size[1]) * obj.rect.height:
+            obj.rect.y += -obj.rect.height * (1 + self.size[1])
 
-    # позиционировать камеру на объекте target
     def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - WIDTH // 2)
-        self.dy = -(target.rect.y + target.rect.h // 2 - HEIGHT // 2)
+        self.dx = -(target.rect.x + target.rect.w // 2 - 500 // 2)
+        self.dy = -(target.rect.y + target.rect.h // 2 - 500 // 2)
 
 
-player, level_x, level_y = generate_level(load_level("level2.txt"))
+images = {'wall': load_image('box.png', -1), 'empty': load_image('grass.png'),
+          'persona': load_image('hamster_lab.png', -1), 'egg': load_image('egg.png', -1),
+          'heart': load_image('heart_lab.png', -1), 'fon': load_image('fon2.jpg')}
+
+tile_width = tile_height = 50
+
+new_game = False
+
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+box_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+heart_group = pygame.sprite.Group()
+
+list_level = ["level.txt", "level2.txt", "level3.txt", "level4.txt", "level5.txt"]
+lev = random.choice(list_level)
+player, level_x, level_y = generate_level(load_level(lev))
+
+camera = Camera((level_x, level_y))
+
 egg = images['egg']
 egg_mask = pygame.mask.from_surface(egg)
+
+score = 0
+font = pygame.font.Font(None, 30)
+text = font.render(str(score), 1, pygame.Color('black'))
 running = True
+moves = None
 
-camera = Camera()
 
+def begin():
+    global running, score, text, new_game, camera, player, all_sprites,\
+        player_group, heart_group, tiles_group, box_group
+    start_screen()
+    while running:
+        if new_game:
+            new_game = False
+            all_sprites = pygame.sprite.Group()
+            tiles_group = pygame.sprite.Group()
+            box_group = pygame.sprite.Group()
+            player_group = pygame.sprite.Group()
+            heart_group = pygame.sprite.Group()
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == K_UP:
-                player.rect = player.rect.move(0, -50)
-            if event.key == K_DOWN:
-                player.rect = player.rect.move(0, +50)
-            if event.key == K_RIGHT:
-                player.rect = player.rect.move(+50, 0)
-            if event.key == K_LEFT:
-                player.rect = player.rect.move(-50, 0)
+            list_level = ["level.txt", "level2.txt", "level3.txt", "level4.txt", "level5.txt"]
+            lev = random.choice(list_level)
+            player, level_x, level_y = generate_level(load_level(lev))
 
-    # изменяем ракурс камеры
-    camera.update(player)
-    # обновляем положение всех спрайтов
-    for sprite in all_sprites:
-        camera.apply(sprite)
+            camera = Camera((level_x, level_y))
 
-    screen.fill((0, 0, 0))
-    tiles_group.draw(screen)
-    player_group.draw(screen)
-    screen.blit(egg, (0, 0))
+            score = 0
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == K_UP:
+                    player.rect = player.rect.move(0, -50)
+                if event.key == K_DOWN:
+                    player.rect = player.rect.move(0, +50)
+                if event.key == K_RIGHT:
+                    player.rect = player.rect.move(+50, 0)
+                if event.key == K_LEFT:
+                    player.rect = player.rect.move(-50, 0)
 
-    pygame.display.flip()
-    clock.tick(FPS)
-# завершение работы:
+        # изменяем ракурс камеры
+        camera.update(player)
+        # обновляем положение всех спрайтов
+        for sprite in all_sprites:
+            camera.apply(sprite)
+            sprite.rect.x += 150
+            sprite.rect.y += 150
+
+        screen.fill((0, 0, 0))
+        all_sprites.draw(screen)
+        player_group.draw(screen)
+        heart_group.draw(screen)
+        pygame.draw.rect(screen, pygame.Color('black'), (0, 0, WIDTH, 255))
+        pygame.draw.rect(screen, pygame.Color('black'), (500, 0, WIDTH - 500, HEIGHT))
+        screen.blit(egg, (0, 0))
+
+        if pygame.sprite.groupcollide(player_group, box_group, False, False):
+            start_screen(game_over=True)
+            new_game = True
+
+        for heart in heart_group:
+            if pygame.sprite.spritecollideany(heart, player_group):
+                heart.kill()
+                score += 1
+
+        text = font.render(str(score), 1, pygame.Color('black'))
+        screen.blit(text, (330, 280))
+
+        pygame.display.flip()
+        clock.tick(FPS)
+    return score
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import random
 import shoes
 import snake
+import labirint
 
 # верунлись границы комнат
 
@@ -225,6 +226,8 @@ def choose_game(mouse_pos, click=False):
         if games[num_game] == 'snake':
             happiness.fill(snake.begin())
             print(happiness.value)
+        if games[num_game] == 'labirint':
+            happiness.fill(labirint.begin())
 
 
 def click_processing(btn):  # вынесла обработку нажатий в отдельную функцию сейчас, т.к. все равно
@@ -348,8 +351,9 @@ system_details_images = {'arrow_left': load_image('arrow_left.png', -1),
                          'display': load_image('egg.png', -1),
                          'soap': load_image('soap.png', -1)}
 
-game_images = {'shoes': load_image('icons/shoes_game.png', -1), 'snake': load_image('icons/snake_game.png', -1)}
-games = ['shoes', 'snake']
+game_images = {'shoes': load_image('icons/shoes_game.png', -1), 'snake': load_image('icons/snake_game.png', -1),
+               'labirint': load_image('icons/labirint_game.png', -1)}
+games = ['shoes', 'snake', 'labirint']
 
 food = [load_image('food/banana.png', -1), load_image('food/egg.png', -1), load_image('food/grapes.png', -1),
         load_image('food/salad.png', -1),
